1b961e00b4a7f20815af525fae2dbeebed728606
blademan82 <webcooking.pro@gmail.com>
2020-02-08 01:57:13 +0200

    add new rule

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
-/*
-GAME RULES:
-
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-*/
-let scores, roundScores, activePlayer, gamePlaying;
+let scores, roundScores, activePlayer, gamePlaying, lastDice;
 init();
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
@@ -16,30 +6,38 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
 
     let dice = Math.floor(Math.random() * 6) + 1;
 
+    console.log(dice);
+    console.log(lastDice);
+
     let diceDOM = document.querySelector('.dice');
     showDice();
     diceDOM.setAttribute('src', `dice-${dice}.png`)
 
-    if (dice > 1) {
+    if (lastDice == 6 && dice == 6) {
 
+      scores[activePlayer] = 0;
+      document.querySelector(`#score-` + activePlayer).textContent = 0;
+      nextPlayer();
+    } else if (dice !== 1) {
       roundScores += dice;
       document.querySelector('#current-' + activePlayer).textContent = roundScores;
-
     } else {
 
       nextPlayer();
 
     }
+    lastDice = dice;
   }
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function () {
   if (gamePlaying) {
     scores[activePlayer] += roundScores;
+    lastDice = 0;
 
     document.querySelector(`#score-` + activePlayer).textContent = scores[activePlayer];
 
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 100) {
 
       document.querySelector(`#name-${activePlayer}`).textContent = 'Winner!';
       document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner');
@@ -58,8 +56,8 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
 function nextPlayer() {
   activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
   roundScores = 0;
-  document.querySelector('#current-0').textContent = '0'
-  document.querySelector('#current-1').textContent = '0'
+  document.querySelector('#current-0').textContent = 0;
+  document.querySelector('#current-1').textContent = 0;
 
   document.querySelector('.player-0-panel').classList.toggle('active');
   document.querySelector('.player-1-panel').classList.toggle('active');
